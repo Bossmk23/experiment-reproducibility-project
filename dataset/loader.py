79d87f912115f2edf6060c7b92242172b9c66a57
Bossmk23 <138993426+Bossmk23@users.noreply.github.com>
2025-07-14 22:39:36 +0530

    Add files via upload

--- dataset/loader.py
+++ dataset/loader.py
@@ -1,1 +1,20 @@
 
+import pandas as pd
+from torch.utils.data import Dataset, DataLoader
+import torch
+
+class IrisDataset(Dataset):
+    def __init__(self, csv_file):
+        df = pd.read_csv(csv_file)
+        self.X = torch.tensor(df.drop("species", axis=1).values, dtype=torch.float32)
+        self.y = torch.tensor(pd.factorize(df["species"])[0], dtype=torch.long)
+
+    def __len__(self):
+        return len(self.X)
+
+    def __getitem__(self, idx):
+        return self.X[idx], self.y[idx]
+
+def get_dataloader(config):
+    dataset = IrisDataset(config["dataset"]["csv_path"])
+    return DataLoader(dataset, batch_size=config["training"]["batch_size"], shuffle=True)
